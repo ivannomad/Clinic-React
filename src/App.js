86d5afb27f9dcaedd47e8e64f2ee5fc2ae413b64
ivannomad <myilinoyd@gmail.com>
2022-01-31 14:23:35 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Register} from "./Register";
+import {Login} from "./Login";
+import {StartPage} from "./StartPage";
+import {Home} from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Route path='/' exact={true} element={<StartPage />}/>
+        <Route path='/register' exact={true} element={<Register />}/>
+        <Route path='/login' exact={true} element={<Login />}/>
+        <Route path='/home' exact={true} element={<Home />}/>
+      </Router>
   );
 }
 
---
+++ src/StartPage.js
@@ -0,0 +1,4 @@
+export function StartPage() {
+  return null;
+}
+
