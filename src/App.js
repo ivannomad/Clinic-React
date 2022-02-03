3e8b28f351739f534ad2540746b0cb2cdd622f95
Denys Slozkin <46857808+Den1613@users.noreply.github.com>
2022-02-03 17:38:15 +0200

    KH05802CLI-59 (#2)
    KH05802CLI-59

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Register} from "./Register";
-import { Login } from "./Login";
-import { StartPage } from "./StartPage";
-import { Home } from "./Home";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {Login} from "./Login";
+import {StartPage} from "./StartPage";
+import {Home} from "./Home";
+import {Register} from "./component/Register";
 
 const App = () => {
-  return (
-      <Router>
-        <Routes>
-          <Route path='/' exact={true} element={<StartPage />}/>
-          <Route path='/register' exact={true} element={<Register />}/>
-          <Route path='/login' exact={true} element={<Login />}/>
-          <Route path='/home' exact={true} element={<Home />}/>
-        </Routes>
-      </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' exact={true} element={<StartPage/>}/>
+                <Route path='/register' exact={true} element={<Register/>}/>
+                <Route path='/login' exact={true} element={<Login/>}/>
+                <Route path='/home' exact={true} element={<Home/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,29 +2,38 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8081/api/v1/auth/';
 
+const register = (email, password) => {
+    return axios
+        .post(API_URL + "register", {
+            'username': email,
+            'password': password
+        });
+};
+
 const login = (email, password) => {
-  return axios
-      .post(API_URL + 'login', { 'username': email, 'password': password })
-      .then(res => {
-        localStorage.setItem('accessToken', res.data.accessToken);
-        console.log(res.data.accessToken);
-        console.log(res);
-      })
-      .catch(error => console.log(error));
+    return axios
+        .post(API_URL + 'login', {'username': email, 'password': password})
+        .then(res => {
+            localStorage.setItem('accessToken', res.data.accessToken);
+            console.log(res.data.accessToken);
+            console.log(res);
+        })
+        .catch(error => console.log(error));
 }
 
 const logout = () => {
-  localStorage.removeItem('user');
+    localStorage.removeItem('user');
 }
 
 const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('user'));
+    return JSON.parse(localStorage.getItem('user'));
 }
 
 const authService = {
-  login,
-  logout,
-  getCurrentUser,
+    login,
+    logout,
+    getCurrentUser,
+    register
 };
 
 export default authService;
