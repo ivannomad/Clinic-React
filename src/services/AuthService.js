be6e6ec940b34a2fc8da82e3c4c787b65f0970c4
ivannomad <54027265+ivannomad@users.noreply.github.com>
2022-02-03 14:59:25 +0200

    KH05802CLI-56 (#1)

---
+++ src/services/AuthService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8081/api/v1/auth/';
+
+const login = (email, password) => {
+  return axios
+      .post(API_URL + 'login', { 'username': email, 'password': password })
+      .then(res => {
+        localStorage.setItem('accessToken', res.data.accessToken);
+        console.log(res.data.accessToken);
+        console.log(res);
+      })
+      .catch(error => console.log(error));
+}
+
+const logout = () => {
+  localStorage.removeItem('user');
+}
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem('user'));
+}
+
+const authService = {
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default authService;
