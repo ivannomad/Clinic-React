484e51652e0a8dff321a7ce98986212ba1595c80
ivannomad <54027265+ivannomad@users.noreply.github.com>
2022-02-04 13:13:24 +0200

    KH05802CLI-57 (#3)

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -11,29 +11,22 @@ const register = (email, password) => {
 };
 
 const login = (email, password) => {
-    return axios
-        .post(API_URL + 'login', {'username': email, 'password': password})
-        .then(res => {
-            localStorage.setItem('accessToken', res.data.accessToken);
-            console.log(res.data.accessToken);
-            console.log(res);
-        })
-        .catch(error => console.log(error));
+  return axios
+      .post(API_URL + 'login', { 'username': email, 'password': password });
 }
 
 const logout = () => {
-    localStorage.removeItem('user');
+  localStorage.removeItem('user');
 }
 
 const getCurrentUser = () => {
-    return JSON.parse(localStorage.getItem('user'));
+  return JSON.parse(localStorage.getItem('user'));
 }
 
 const authService = {
-    login,
-    logout,
-    getCurrentUser,
-    register
+  login,
+  logout,
+  getCurrentUser,
 };
 
 export default authService;
