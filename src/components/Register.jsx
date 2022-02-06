8fec5d8b3b382d1975abe2e9599a20a4ccad6a61
Denys Slozkin <46857808+Den1613@users.noreply.github.com>
2022-02-06 14:27:13 +0200

    KH05802CLI-60 (#6)

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,47 +4,94 @@ import AuthService from "../services/AuthService";
 import {useNavigate} from 'react-router-dom';
 
 export const Register = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [responseStatus, setResponseStatus] = useState(null)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [secondName, setSecondName] = useState("")
+  const [contactNumber, setContactNumber] = useState("")
+  const [birthDate, setBirthDate] = useState("")
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        AuthService.register(email, password)
-            .then(res => {
-                localStorage.setItem("user", JSON.stringify(res.data));
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-                navigate('/profile');
-                window.location.reload();
-            })
-            .catch(error => {
-                setResponseStatus(error.response.status)
-            })
-    }
+  const handleSubmit = async e => {
+    e.preventDefault();
 
-    return (
-        <Container id="main-container" className="d-grid h-100">
-            <Form id="sign-up-form" className="text-center p-3 w-100" onSubmit={handleSubmit}>
-                <h1 className="mb-3 fs-3 fw-normal">Please sign up</h1>
-                <Form.Group controlId="sign-up-email-address">
-                    <Form.Control required type="email" size="lg" placeholder="Email address"
-                                  className="position-relative" onChange={event => setEmail(event.target.value)}/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="sign-up-password">
-                    <Form.Control required type="password" size="lg" placeholder="Password"
-                                  className="position-relative" onChange={event => setPassword(event.target.value)}/>
-                </Form.Group>
-                <div className="d-grid">
-                    <Button className="mb-3" variant="primary" size="lg" type="submit">Sign Up</Button>
-                </div>
-                {responseStatus === 409 && (
-                    <Alert variant="danger">
-                        <Alert.Heading>This email is already in use!</Alert.Heading>
-                    </Alert>
-                )}
-            </Form>
-        </Container>
-    );
+    setLoading(true);
+
+    await AuthService.register(email, password)
+        .catch(error => {
+          const resMessage =
+              (error.response &&
+                  error.response.data &&
+                  error.response.data.message) ||
+              error.message ||
+              error.toString();
+          setLoading(false);
+          setMessage(resMessage);
+        })
+
+    await AuthService.postRegister(firstName, secondName, contactNumber, birthDate)
+        .then(() => {
+          navigate('/profile')
+        })
+        .catch(error => {
+          const resMessage =
+              (error.response &&
+                  error.response.data &&
+                  error.response.data.message) ||
+              error.message ||
+              error.toString();
+          setLoading(false);
+          setMessage(resMessage);
+        })
+  }
+
+  return (
+      <Container id="main-container" className="d-grid h-100">
+        <Form id="sign-up-form" className="text-center p-3 w-100" onSubmit={handleSubmit}>
+          <h1 className="mb-3 fs-3 fw-normal">Please sign up</h1>
+          <Form.Group controlId="sign-up-email-address">
+            <Form.Control required type="email" size="lg" placeholder="Email address"
+                          className="position-relative" onChange={event => setEmail(event.target.value)}/>
+          </Form.Group>
+          <Form.Group controlId="sign-up-password">
+            <Form.Control required type="password" size="lg" placeholder="Password"
+                          className="position-relative" onChange={event => setPassword(event.target.value)}/>
+          </Form.Group>
+          <Form.Group controlId="sign-up-first-name">
+            <Form.Control required type="text" size="lg" placeholder="First Name"
+                          className="position-relative" onChange={event => setFirstName(event.target.value)}/>
+          </Form.Group>
+          <Form.Group controlId="sign-up-second-name">
+            <Form.Control required type="text" size="lg" placeholder="Second Name"
+                          className="position-relative" onChange={event => setSecondName(event.target.value)}/>
+          </Form.Group>
+          <Form.Group controlId="sign-up-contact-number">
+            <Form.Control required type="text" size="lg" placeholder="Contact Number"
+                          className="position-relative"
+                          onChange={event => setContactNumber(event.target.value)}/>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="sign-up-birth-date">
+            <Form.Control required type="date" size="lg" placeholder="Birth Date"
+                          className="position-relative" onChange={event => setBirthDate(event.target.value)}/>
+          </Form.Group>
+          <div className="d-grid">
+            <Button className="mb-3" variant="primary" size="lg" type="submit">
+              {loading && (
+                  <span className="spinner-border spinner-border-sm"/>
+              )}
+              Sign Up
+            </Button>
+          </div>
+          {message && (
+              <Alert variant="danger">
+                <Alert.Heading>{message}</Alert.Heading>
+              </Alert>
+          )}
+        </Form>
+      </Container>
+  );
 }
--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,19 +1,39 @@
 import axios from 'axios';
 import JwtDecodeService from "./JwtService";
+import authHeader from "./AuthHeader";
 
 const API_URL = 'http://localhost:8081/api/v1/auth/';
 
 const register = (email, password) => {
-    return axios
-        .post(API_URL + "register", {
-            'username': email,
-            'password': password
-        });
+  return axios
+      .post(API_URL + 'register', {
+        'username': email,
+        'password': password
+      })
+      .then(res => {
+        if (res.data.accessToken) {
+          JwtDecodeService.saveUser(res.data.accessToken)
+        }
+      });
+};
+
+const postRegister = (firstName, secondName, contactNumber, birthDate) => {
+  return axios
+      .post("http://localhost:8080/patients", {
+        'id': getCurrentUser().userId,
+        'firstName': firstName,
+        'secondName': secondName,
+        'contactNumber': contactNumber,
+        'birthDate': birthDate,
+      }, {headers: authHeader()})
 };
 
 const login = (email, password) => {
   return axios
-      .post(API_URL + 'login', { 'username': email, 'password': password })
+      .post(API_URL + 'login', {
+        'username': email,
+        'password': password
+      })
       .then(res => {
         if (res.data.accessToken) {
           JwtDecodeService.saveUser(res.data.accessToken)
@@ -30,6 +50,7 @@ const getCurrentUser = () => {
 }
 
 const authService = {
+  postRegister,
   register,
   login,
   logout,
