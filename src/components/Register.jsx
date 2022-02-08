c096f9f5b3248bfabd9bf2c730b21ab233b72d0b
Den1613 <gurunaax@gmail.com>
2022-02-08 15:12:08 +0200

    KH05802CLI-67

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import {Alert, Button, Container, Form} from "react-bootstrap";
+import {Alert, Button, Container, FloatingLabel, Form} from "react-bootstrap";
 import AuthService from "../services/AuthService";
 import {useNavigate} from 'react-router-dom';
 
@@ -51,34 +51,33 @@ export const Register = () => {
   }
 
   return (
-      <Container id="main-container" className="d-grid h-100">
-        <Form id="sign-up-form" className="text-center p-3 w-100" onSubmit={handleSubmit}>
+      <Container id="auth-container" className="d-grid h-100">
+        <Form id="auth-form" className="text-center p-3 w-100" onSubmit={handleSubmit}>
           <h1 className="mb-3 fs-3 fw-normal">Please sign up</h1>
-          <Form.Group controlId="sign-up-email-address">
-            <Form.Control required type="email" size="lg" placeholder="Email address"
-                          className="position-relative" onChange={event => setEmail(event.target.value)}/>
-          </Form.Group>
-          <Form.Group controlId="sign-up-password">
-            <Form.Control required type="password" size="lg" placeholder="Password"
-                          className="position-relative" onChange={event => setPassword(event.target.value)}/>
-          </Form.Group>
-          <Form.Group controlId="sign-up-first-name">
-            <Form.Control required type="text" size="lg" placeholder="First Name"
-                          className="position-relative" onChange={event => setFirstName(event.target.value)}/>
-          </Form.Group>
-          <Form.Group controlId="sign-up-second-name">
-            <Form.Control required type="text" size="lg" placeholder="Second Name"
-                          className="position-relative" onChange={event => setSecondName(event.target.value)}/>
-          </Form.Group>
-          <Form.Group controlId="sign-up-contact-number">
-            <Form.Control required type="text" size="lg" placeholder="Contact Number"
-                          className="position-relative"
+          <FloatingLabel label="Email address" className="mb-2">
+            <Form.Control required type="email" placeholder="Email address"
+                          onChange={event => setEmail(event.target.value)}/>
+          </FloatingLabel>
+          <FloatingLabel label="Password" className="mb-2">
+            <Form.Control required type="password" placeholder="Password"
+                          onChange={event => setPassword(event.target.value)}/>
+          </FloatingLabel>
+          <FloatingLabel label="First Name" className="mb-2">
+            <Form.Control required type="text" placeholder="First Name"
+                          onChange={event => setFirstName(event.target.value)}/>
+          </FloatingLabel>
+          <FloatingLabel label="Second Name" className="mb-2">
+            <Form.Control required type="text" placeholder="Second Name"
+                          onChange={event => setSecondName(event.target.value)}/>
+          </FloatingLabel>
+          <FloatingLabel label="Contact Number" className="mb-2">
+            <Form.Control required type="text" placeholder="Contact Number"
                           onChange={event => setContactNumber(event.target.value)}/>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="sign-up-birth-date">
-            <Form.Control required type="date" size="lg" placeholder="Birth Date"
-                          className="position-relative" onChange={event => setBirthDate(event.target.value)}/>
-          </Form.Group>
+          </FloatingLabel>
+          <FloatingLabel label="Birth Date" className="mb-2">
+            <Form.Control required type="date" placeholder="Birth Date"
+                           onChange={event => setBirthDate(event.target.value)}/>
+          </FloatingLabel>
           <div className="d-grid">
             <Button className="mb-3" variant="primary" size="lg" type="submit">
               {loading && (
