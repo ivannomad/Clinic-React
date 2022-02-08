f8d87322efe90834ac392bba95ca7261f9bb71ec
Denys Slozkin <46857808+Den1613@users.noreply.github.com>
2022-02-08 15:12:41 +0200

    KH05802CLI-67 (#10)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { Alert, Button, Container, Form } from "react-bootstrap";
+import React, {useState} from "react";
+import {Alert, Button, Container, FloatingLabel, Form} from "react-bootstrap";
 import AuthService from "../services/AuthService";
-import { useNavigate } from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -14,13 +14,12 @@ export const Login = () => {
   const handleLogin = e => {
     e.preventDefault();
 
-    setMessage("");
     setLoading(true);
 
     AuthService.login(email, password)
         .then(() => {
-              navigate('/profile');
-              window.location.reload();
+          navigate('/profile');
+          window.location.reload();
         })
         .catch(error => {
           const resMessage =
@@ -36,36 +35,31 @@ export const Login = () => {
   };
 
   return (
-    <Container id="main-container" className="d-grid h-100">
-      <Form id="sign-up-form" className="text-center p-3 w-100" onSubmit={handleLogin}>
-        <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
-
-        <Form.Group controlId="sign-up-email-address">
-          <Form.Control required type="email" size="lg" placeholder="Email address"
-                        className="position-relative" onChange={event => setEmail(event.target.value)}/>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="sign-up-password">
-          <Form.Control required type="password" size="lg" placeholder="Password"
-                        className="position-relative" onChange={event => setPassword(event.target.value)}/>
-        </Form.Group>
-
-        <div className="d-grid">
-          <Button className="mb-3" variant="primary" size="lg" type="submit" disabled={loading}>
-            {loading && (
-                <span className="spinner-border spinner-border-sm"></span>
-            )}
-            Sign In
-          </Button>
-        </div>
-
-        {message && (
-            <Alert variant="danger">
-              <Alert.Heading>{message}</Alert.Heading>
-            </Alert>
-        )}
-
-      </Form>
-    </Container>
+      <Container id="auth-container" className="d-grid h-100">
+        <Form id="auth-form" className="text-center p-3 w-100" onSubmit={handleLogin}>
+          <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
+          <FloatingLabel label="Email address" className="mb-2">
+            <Form.Control required type="email" placeholder="Email address"
+                          onChange={event => setEmail(event.target.value)}/>
+          </FloatingLabel>
+          <FloatingLabel label="Password" className="mb-2">
+            <Form.Control required type="password" placeholder="Password"
+                          onChange={event => setPassword(event.target.value)}/>
+          </FloatingLabel>
+          <div className="d-grid">
+            <Button className="mb-3" variant="primary" size="lg" type="submit" disabled={loading}>
+              {loading && (
+                  <span className="spinner-border spinner-border-sm"/>
+              )}
+              Sign In
+            </Button>
+          </div>
+          {message && (
+              <Alert variant="danger">
+                <Alert.Heading>{message}</Alert.Heading>
+              </Alert>
+          )}
+        </Form>
+      </Container>
   );
 }
