789386d602dbe1859459a7ea74aa682fc3ed69b8
Denys Slozkin <46857808+Den1613@users.noreply.github.com>
2022-02-09 01:39:46 +0200

    KH05802 CLI 63 (#14)
    * KH05802CLI-64
    
    * KH05802CLI-63

--- src/components/patient/PatientAppointments.jsx
+++ src/components/patient/PatientAppointments.jsx
@@ -1,13 +1,50 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import {Button, Card, Col, Container, Row} from "react-bootstrap";
+import authHeader from "../../services/AuthHeader";
+import AuthService from "../../services/AuthService";
 
 export const PatientAppointments = () => {
 
-  useEffect(async () => {
-    await axios.get("http://localhost:8080", {})
-  }, [])
+    const currentUser = AuthService.getCurrentUser();
+    const [appointments, setAppointments] = useState([]);
 
-  return (
-      <div>Hello</div>
-  )
+    useEffect(() => {
+        axios.get("http://localhost:8080/patients/" + currentUser.userId + "/appointments", {headers: authHeader()})
+            .then(res => setAppointments(res.data))
+            .catch(error => console.log(error))
+    }, [appointments])
+
+    const handleButton = async e => {
+        e.preventDefault();
+        await axios.delete("http://localhost:8080/patients/" + currentUser.userId + "/appointments/" + e.target.value + "/cancel", {headers: authHeader()})
+    };
+
+    return (
+        <Container>
+            <Row xs={1} md={2} className="g-4 mt-0">
+                {appointments.map(appointment =>
+                    <Col key={appointment.id}>
+                        <Card bg={"light"}>
+                            <Card.Body>
+                                <Card.Title>Appointment</Card.Title>
+                                <Card.Text>
+                                    <div className="mt-2">
+                                        <strong>Doctor: </strong> {appointment.doctor.firstName + " " + appointment.doctor.secondName}
+                                    </div>
+                                    <div className="mt-2">
+                                        <strong>Date: </strong> {appointment.dateAndTime}
+                                    </div>
+                                    <div className="mt-2">
+                                        <Button variant={"danger"} value={appointment.id}
+                                                onClick={handleButton}>Cancel</Button>
+                                    </div>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                )}
+            </Row>
+        </Container>
+    )
 }
